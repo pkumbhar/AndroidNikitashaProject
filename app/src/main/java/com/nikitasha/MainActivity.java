5d97b9c5458f2ce725903d6763930f2e1e96950d
pkumbhar <pratim.kumbhar4@gmail.com>
2017-07-27 18:36:21 +0530

    Initial commit

---
+++ app/src/main/java/com/nikitasha/MainActivity.java
@@ -0,0 +1,27 @@
+package com.nikitasha;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    private void setHome(){
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+}
